ac34d51ef9dc7970eaf9cc52cc2cfb179351783c
Oliver Atom <atom@olivers-mac.local>
2015-10-11 22:07:40 -0500

    Agregados logs para entender app lifecycle

--- Hell ios/ViewController.swift
+++ Hell ios/ViewController.swift
@@ -13,11 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("view did load")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        print("did recieve memory warnig")
     }
 
 
